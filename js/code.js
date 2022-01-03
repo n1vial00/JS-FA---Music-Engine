89d957b0e4a466afd25288ba13a8ecf71dff2062
Aleksi Viitanen <n1vial00@students.oamk.fi>
2022-01-03 14:56:27 +0200

    Tyyli viimeistelty. Nav toimii, mutta turha.

--- js/code.js
+++ js/code.js
@@ -2,37 +2,11 @@
 
 let API = "bd48b2b48c1b2e96b40249c9434bb1d8";
 
-function songSearch() {
+ARTIST_LIST = ["Carpenter Brut", "Cheshyre", "Turmion Kätilöt", "Eluveitie", "Eisbrecher", "The Crash"];
+SONG_LIST = ["Turbo Killer", "Animal", "Hades", "Lvgvs", "1000 Flammen", "Lauren Caught My Eye"];
 
-    document.querySelector("#getSongContent").textContent = "";
-    let artist = document.querySelector("#songInI").value;
-    let song = document.querySelector("#songInII").value;
-
-    if(artist === "" || song === "") {
-        document.querySelector("#getSongContent").textContent = "Error! Insert both an artist and a song. Using default values.";
-        artist = "Carpenter Brut";
-        song = "Turbo Killer";
-    }
-
-
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET","https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=" + artist + "&track=" + song + "&autocorrect=1&limit=10&api_key=" + API + "&format=json", true);
-
-    xmlhttp.send();
-
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let div = document.createElement("div");
-            for(i = 0; i <= 9; i++) {
-            let answerObj = JSON.parse(xmlhttp.response);
-            answerObj = answerObj.similartracks.track;
-            let div2 = document.createElement("div");
-            div2.textContent = answerObj[i].artist.name + " - " + answerObj[i].name;
-            div.append(div2);
-            document.querySelector("#getSongContent").append(div);
-            }
-        }
-    }
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
 }
 
 function artistSearch() {
@@ -42,14 +16,12 @@ function artistSearch() {
 
     if(artist === "") {
         document.querySelector("#artistInfoBox").textContent = "Error! Insert both an artist and a song. Using default values.";
-        artist = "Carpenter Brut";
+        artist = ARTIST_LIST[getRndInteger(0, ARTIST_LIST.length)];
     }
 
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET","https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + artist + "&autocorrect=1&limit=10&api_key=" + API + "&format=json", true);
-
     xmlhttp.send();
-
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let div = document.createElement("div");
@@ -58,7 +30,7 @@ function artistSearch() {
                 answerObj = answerObj.toptracks.track;
                 let div2 = document.createElement("div");
                 if(i == -1) {
-                    div2.textContent = "Artist: " + answerObj[0].artist.name;
+                    div2.textContent = 'Artist: "' + answerObj[0].artist.name + '"';
                     div2.classList.add("artistDiv")
                 } else {
                     div2.textContent = answerObj[i].name;
@@ -68,11 +40,51 @@ function artistSearch() {
                 document.querySelector("#artistInfoBox").append(div);
             }
         }
+    } 
+}
+
+function songSearch() {
+
+    document.querySelector("#getSongContent").textContent = "";
+    let artist = document.querySelector("#songInI").value;
+    let song = document.querySelector("#songInII").value;
+
+    if(artist === "" || song === "") {
+        document.querySelector("#getSongContent").textContent = "Error! Insert both an artist and a song. Using default values.";
+        let rndNr = getRndInteger(0, ARTIST_LIST.length);
+        artist = ARTIST_LIST[rndNr];
+        song = SONG_LIST[rndNr];
     }
-    
+
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET","https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=" + artist + "&track=" + song + "&autocorrect=1&limit=10&api_key=" + API + "&format=json", true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let div = document.createElement("div");
+            for(i = -1; i <= 9; i++) {
+                let answerObj = JSON.parse(xmlhttp.response);
+                let answerObjX = answerObj.similartracks.track;
+                let div2 = document.createElement("div");
+                if(i == -1) {
+                    div2.textContent = 'Finding songs similar to "' + song + '" by "' + artist + '"';
+                    div2.classList.add("artistDiv");
+                } else {
+                    div2.textContent = answerObjX[i].artist.name + " - " + answerObjX[i].name; 
+                }
+                console.log(answerObjX);
+                div.append(div2);
+                document.querySelector("#getSongContent").append(div);
+            }
+        }
+    }
+}
+
+function toggle() {
+    document.querySelector("#hid").classList.toggle("hiddenOnes")
 }
 
 
-
-document.querySelector("#songBut").addEventListener("click", songSearch);
 document.querySelector("#artistBut").addEventListener("click", artistSearch);
+document.querySelector("#songBut").addEventListener("click", songSearch);
+
